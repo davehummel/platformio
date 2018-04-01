ec34cc39d417fcec6bf259a3f03f7ae2374fe206
Dave Hummel <dmhummel@gmail.com>
2018-04-01 09:45:08 -0500

    Improved control of PH Sensor

--- fishchemsensor/src/main.cpp
+++ fishchemsensor/src/main.cpp
@@ -3,7 +3,7 @@
 
 #include "SPI.h"
 
-#include "dh_atlas_ph_i2c.h"
+#include "dh_atlas_i2c.h"
 
 #include "dh_rf24client.h"
 #include <EEPROM.h>
@@ -13,7 +13,11 @@
 #define WRITE_RESTART(val) ((*(volatile uint32_t *)RESTART_ADDR) = (val))
 
 RF24Client rfClient(14,10);
-PHSensorI2C sensor;
+
+AtlasSensorI2C phSensor(99);
+AtlasSensorI2C salinitySensor(100);
+AtlasSensorI2C orpSensor(98);
+AtlasSensorI2C doSensor(97);
 
 byte clientName[5]  = {DEFCLIENTNAME};
 
@@ -39,7 +43,11 @@ void setup()
   Wire.setDefaultTimeout(2000);
   Wire.begin();
 
-  sensor.setWire(&Wire);
+  phSensor.setWire(&Wire);
+  doSensor.setWire(&Wire);
+  orpSensor.setWire(&Wire);
+  salinitySensor.setWire(&Wire);
+
 
   SPI.setSCK(13);
 
@@ -75,12 +83,14 @@ void setup()
 
 uint8_t failedCount = 0;
 elapsedMillis countDown = 0;
+uint8_t sensorCount = 0;
 void loop()
 {
   delay(10);
   rfClient.listen();
-  if (Serial.available()>=5){
-    if (Serial.read() == '@'){
+  if (Serial.available()){
+    char in = Serial.read();
+    if (in == '@'){
       uint8_t prefix [4];
       prefix[0] = Serial.read();
       prefix[1] = Serial.read();
@@ -89,6 +99,35 @@ void loop()
       rfClient.saveClientPrefixToEEPROM(prefix);
       Serial.println("Client Prefix saved to eeprom");
       resetSensor(2);
+    } else if (in == '#'){
+      uint8_t devNum = Serial.read()-'0';
+      char input[20] = {0};
+      uint8_t i = 0;
+      while (i<29){
+        input[i] = Serial.read();
+        if (input[i] == '\n' || input[i] == '\r'){
+          break;
+        }
+        i++;
+      }
+      input[i] = '\0';
+      Serial.print("Sending command:");
+      Serial.print(input);
+      Serial.print(" to device ");
+      Serial.print(devNum);
+      switch(devNum){
+
+        case 0: phSensor.sendCommand(input,input);Serial.println(" = ph"); break;
+        case 1: salinitySensor.sendCommand(input, input);Serial.println(" = salinity"); break;
+        case 2: orpSensor.sendCommand(input,input);Serial.println(" = orp"); break;
+        case 3: doSensor.sendCommand(input, input);Serial.println(" = do"); break;
+        default: Serial.println(" = unknown");
+
+      }
+
+      Serial.print("Response:");
+      Serial.println(input);
+
     } else {
       while (Serial.available()) Serial.read();
     }
@@ -96,28 +135,40 @@ void loop()
 
   if (countDown>=1000){
       Serial.println("Attempting Read");
-    countDown = countDown - 1000;
+      countDown = countDown - 1000;
 
-    double ph = sensor.readMeasurement();
-    if (ph == -1){
-      //failed once;
-      Serial.println("Failed Read");
-        delay( 500);
+      double in = 0;
+      if (sensorCount == 0)
+        in = phSensor.readMeasurement();
+      else if (sensorCount == 1)
+        in = salinitySensor.readMeasurement();
+      else if (sensorCount == 2)
+        in = orpSensor.readMeasurement();
+      else if (sensorCount == 3)
+        in = doSensor.readMeasurement();
 
-      failedCount++;
+      if (in == -9999){
+        //failed once;
+        Serial.println("Failed Read");
+          delay( 1000);
 
-      if (failedCount >= 5){
+        failedCount++;
+
+        if (failedCount >= 10){
+          failedCount = 0;
+          resetSensor(2);
+        }
+
+      }else{
+        // all good!
+        Serial.print("Got Sensor(");
+        Serial.print(sensorCount);
+        Serial.print(")=");
+        Serial.println(in);
+        rfClient.sendDouble(sensorCount,in);
         failedCount = 0;
-        resetSensor(1);
       }
-
-    }else{
-      // all good!
-      Serial.print("Got PH:");
-      Serial.println(ph);
-      rfClient.sendDouble(0,ph);
-      failedCount = 0;
-    }
-
+      sensorCount++;
+      if (sensorCount == 4) sensorCount = 0;
   }
 }
