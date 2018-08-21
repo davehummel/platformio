ac3d6f4202919b622bc72a360afd2c778a06ac7a
Dave Hummel <dmhummel@gmail.com>
2018-08-20 21:21:41 -0500

    no message

--- fishtank/src/main.cpp
+++ fishtank/src/main.cpp
@@ -86,11 +86,7 @@ void setup()
   controller.run(2, Controller::newString("D CAA 10"), 'R', 2);
   controller.run(2, Controller::newString("D CBB 10"), 'R', 2);
 
-  for (uint8_t i = 0; i < 20; i++)
-  {
-    digitalWrite(13, i % 2);
-    delay(250);
-  }
+  Serial1.println("Starting control loop");
 }
 
 void loop()
